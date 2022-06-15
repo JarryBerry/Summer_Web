18fe537ff1686a6e08e9f703e84d9a00bb503dce
Jarrett Mohler <jmohler4@kent.edu>
2022-06-15 01:10:51 +0000

    added hello function

--- app.py
+++ app.py
@@ -6,4 +6,6 @@ app = Flask(__name__)
 def get_index():
     return "<p>This is in my index function!!!!!!!!!!!!!</p>"
 
-    
+@app.route("/hello")
+def get_hello():
+    return "<p>Hello and Welcome to Flask!!!!</p>"
