05fc29a5bf0860e5f2920ea7b328422d51ee9978
Jarrett <jmohler4@kent.edu>
2022-06-23 21:05:04 -0400

    password stuff

--- 02-basic-cookies/app.py
+++ 02-basic-cookies/app.py
@@ -1,59 +1,43 @@
 from flask import Flask, session
 from flask import render_template
 from flask import request, redirect, url_for
-import json
-import werkzeug
-from werkzeug.security import generate_password_hash, check_password_hash
 
 app = Flask(__name__)
-app.secret_key="ksmdflkji240[i2hjfsklnf"
 
 username = "stranger"
 
 @app.route("/")
 @app.route("/home")
 def get_index():
-    if 'username' in session:
-        username = session['username']
-    else:
+    global username
+    if username == None:
         return redirect(url_for('get_login'))
     return render_template('home.html', name=username)
 
 @app.route("/other")
 def get_other():
-    if 'username' in session:
-        username = session['username']
-    else:
+    global username
+    if username == None:
         return redirect(url_for('get_login'))
     return render_template('other.html', name=username)
 
 @app.route("/login", methods=['GET'])
 def get_login():
-    if username in session:
+    global username
+    if username != "stranger":
         return redirect(url_for('get_index'))
     return render_template('login.html')
 
 @app.route("/login", methods=['POST'])
 def post_login():
+    global username
     username = request.form.get("username", None)
-    if username != None:
-        return redirect(url_for('get_login'))
-    try:
-        with(f"storage/{username}.json", "r") as f:
-            creds = json.load(f)
-    except Exception as e:
-        print(f"Error in reading credentials! {e}")
-        return redirect(url_for('get_login'))
-    password = request.form.get("password", None)
-    if not check_password_hash(creds['password'], password):
-        print(f"Error - Bad password!")
-        return redirect(url_for('get_login'))
-    session['username'] = username
     return redirect(url_for('get_index'))
 
 @app.route("/logout", methods=['GET'])
 def get_logout():
-    session.pop('username',None)
+    global username
+    username = None
     return redirect(url_for('get_login'))
 
 @app.route("/register", methods=['GET'])
@@ -67,15 +51,8 @@ def post_register():
     username = request.form.get("username", None)
     if username == None:
         return redirect(url_for('get_register'))
-    for c in username.lower():
-        if not(c.isalpha() or c.isdigit() or (c in '.-_')):
-            print("Illegal characters in username!")
-            return redirect(url_for('get_register'))
     password = request.form.get("password", None)
     if password == None:
-        return redirect(url_for('get_register'))
-    if len(password) < 8:
-        print("Password is not long enough!")
         return redirect(url_for('get_register'))
     repeated = request.form.get("repeated", None)
     if repeated == None:
@@ -83,23 +60,9 @@ def post_register():
     if repeated != password:
         print("Repeated password does not match password!")
         return redirect(url_for('get_register'))
-
     # TODO: Do we have a user with this name already?
-    try:
-        with open(f"storage/{username}.json", "r") as f:
-            creds = json.load(f)
-            print("User already exists!")
-            return redirect(url_for('get_register'))
-    except Exception as e:
-        pass
 
     # TODO: Store the user credentials
-    creds = {
-        "username":username,
-        "password":generate_password_hash(password)
-    }
-    with open(f"storage/{username}.json","w") as f:
-        json.dump(creds, f)
 
     # return the logged-in user to a session
     session['username'] = username
--- 03-passwords/app.py
+++ 03-passwords/app.py
@@ -37,9 +37,13 @@ def get_login():
 def post_login():
     username = request.form.get("username", None)
     if username != None:
+        session['username'] = username
+        return redirect(url_for('get_index'))
+    else:
         return redirect(url_for('get_login'))
+
     try:
-        with(f"storage/{username}.json", "r") as f:
+        with(f"storage/{username}.json","r") as f:
             creds = json.load(f)
     except Exception as e:
         print(f"Error in reading credentials! {e}")
@@ -48,17 +52,19 @@ def post_login():
     if not check_password_hash(creds['password'], password):
         print(f"Error - Bad password!")
         return redirect(url_for('get_login'))
-    session['username'] = username
-    return redirect(url_for('get_index'))
+
+        return redirect(url_for('get_index'))
 
 @app.route("/logout", methods=['GET'])
 def get_logout():
-    session.pop('username',None)
+    global username
+    username = None
     return redirect(url_for('get_login'))
 
 @app.route("/register", methods=['GET'])
 def get_register():
     if 'username' in session:
+
         return redirect(url_for('get_index'))
     return render_template('register.html')
 
@@ -68,7 +74,7 @@ def post_register():
     if username == None:
         return redirect(url_for('get_register'))
     for c in username.lower():
-        if not(c.isalpha() or c.isdigit() or (c in '.-_')):
+        if not((c.isalpha() or c.isdigitc) or (c in '.-_')):
             print("Illegal characters in username!")
             return redirect(url_for('get_register'))
     password = request.form.get("password", None)
@@ -83,15 +89,15 @@ def post_register():
     if repeated != password:
         print("Repeated password does not match password!")
         return redirect(url_for('get_register'))
-
     # TODO: Do we have a user with this name already?
     try:
-        with open(f"storage/{username}.json", "r") as f:
+        with(f"storage/{username}.json","r") as f:
             creds = json.load(f)
             print("User already exists!")
-            return redirect(url_for('get_register'))
+            return redirect(url_for('get_index'))
     except Exception as e:
-        pass
+        print(f"Error in reading credentials! {e}")
+        return redirect(url_for('get_login'))
 
     # TODO: Store the user credentials
     creds = {
